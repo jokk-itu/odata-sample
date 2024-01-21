7879be5bf26e20b507c0cbd8925d14b2970ca62a
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2024-01-21 22:35:09 +0100

    add: odata endpoints for product and customer

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Api.Models;
+using Api.Store;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace Api.Controllers;
+public class ProductController : ODataController
+{
+  private readonly IDataStore _dataStore;
+
+  public ProductController(IDataStore dataStore)
+  {
+    _dataStore = dataStore;
+  }
+
+  [EnableQuery]
+  [HttpGet]
+  public ActionResult<IQueryable<Product>> Get()
+  {
+    return Ok(_dataStore.Products);
+  }
+}
